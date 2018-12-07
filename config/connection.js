e982ae68854117820d12c8ab97f1968006275ee0
ohmez <kkid_92@hotmail.com>
2018-12-06 17:15:14 -0700

    heroku + database ready for testing

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+if(process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "bebsburgers"
-});
+}else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bebsburgers"
+  });
+
+}
 
 connection.connect(function(err) {
   if (err) {
